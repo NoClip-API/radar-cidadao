5dd4969e9785487bd7e7bde2074586c735e20d63
Pedro <pedropereirarodrigues16@gmail.com>
2026-05-01 18:49:29 -0300

    refactor: dados dos deputados
    Refeita a forma a qual todos os dados são requeridos

--- src/utils/get_votos_deputado.py
+++
@@ -1,7 +0,0 @@
-import json
-
-def get_votos_deputado(deputado_id):
-    with open(f'src/static/votos_deputados/votos_deputado_{deputado_id}.json', 'r', encoding='utf-8') as f:
-        votos_deputados = json.load(f)
-    
-    return votos_deputados
