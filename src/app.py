6876c3ce267913fa10640ddf57e4d2cf1c7ba885
RafaelMioniC <soundproof4756@gmail.com>
2026-05-03 14:59:24 -0300

    feat: Valores presença do deputado

---
+++ src/app.py
@@ -0,0 +1,81 @@
+from flask import Flask, render_template, request, redirect, url_for
+from utils.functions import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    deputados = get_deputados()
+    partidos = [
+        "AVANTE",
+        "CIDADANIA",
+        "MISSÃO",
+        "NOVO",
+        "PCdoB",
+        "PDT",
+        "PL",
+        "PODE",
+        "PP",
+        "PRD",
+        "PSB",
+        "PSD",
+        "PSDB",
+        "PSOL",
+        "PT",
+        "PV",
+        "REDE",
+        "REPUBLICANOS",
+        "SOLIDARIEDADE",
+        "UNIÃO"
+    ]
+
+    nome = request.args.get('nome')
+    estado = request.args.get('siglaUf')
+    partido = request.args.get('siglaPartido')
+    page = request.args.get('page', 1, type=int)
+
+    if nome or estado or partido:
+        deputados = [
+            d for d in deputados
+            if (not nome or nome.lower() in d['ultimoStatus']['nome'].lower())
+            and (not estado or d['ultimoStatus']['siglaUf'] == estado)
+            and (not partido or d['ultimoStatus']['siglaPartido'] == partido)
+        ]
+
+    per_page = 24
+    inicio = (page - 1) * per_page
+    fim = inicio + per_page
+
+    total = len(deputados)
+    total_pages = (total + per_page - 1) // per_page
+
+    cur_deputados = deputados[inicio:fim]
+
+    return render_template(
+        'index.html',
+        deputados=cur_deputados,
+        partidos=partidos,
+        page=page,
+        total_pages=total_pages,
+        nome=nome,
+        estado=estado,
+        partido=partido
+)
+@app.route('/graficos')
+def graficos():
+    return render_template('graficos.html')
+
+@app.route('/deputado/<id_deputado>')
+def page_deputado(id_deputado):
+    deputado = get_deputado_by_id(id_deputado)
+    votos = get_votos_deputado(id_deputado)
+    grafico_gastos = get_grafico(id_deputado)
+    presenca,_ = get_deputado_presenca(id_deputado)
+    _,eventos = get_deputado_presenca(id_deputado)
+
+    
+
+    return render_template('deputado.html', deputado=deputado, votos=votos, grafico_gastos=grafico_gastos, presenca=presenca, eventos=eventos)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ src/utils/functions.py
@@ -0,0 +1,83 @@
+import json
+import plotly.graph_objects as go
+import plotly.io as pio
+import os
+
+DATA_PATH = os.path.join(os.path.dirname(__file__), '..', 'static', 'data')
+
+# Funções dos deputados
+def get_deputados():
+  deputados = []
+
+  for arquivo in os.listdir(f'{DATA_PATH}/dados_deputados'):
+    if arquivo.startswith('deputado_') and arquivo.endswith('.json'):
+      with open(f'{DATA_PATH}/dados_deputados/{arquivo}', 'r', encoding='utf-8') as f:
+        deputado = json.load(f)
+        deputados.append(deputado)
+
+  deputados.sort(key=lambda x: x['ultimoStatus']['nome'].lower())
+
+  return deputados
+
+def get_deputado_by_id(id: int):
+  with open(f'{DATA_PATH}/dados_deputados/deputado_{id}.json', 'r', encoding='utf-8') as f:
+    deputado = json.load(f)
+    
+    return deputado
+
+def get_gastos_deputado(deputado_id: int):
+  with open(f'{DATA_PATH}/gastos_deputados/gastos_deputado_{deputado_id}.json', 'r', encoding='utf-8') as f:
+    gastos_deputados = json.load(f)
+    
+    return gastos_deputados
+
+def get_votos_deputado(deputado_id):
+    with open(f'{DATA_PATH}/votos_deputados/votos_deputado_{deputado_id}.json', 'r', encoding='utf-8') as f:
+        votos_deputados = json.load(f)
+    
+    return votos_deputados
+
+# Funções dos gráficos
+def get_grafico(id_deputado):
+  gastos = get_gastos_deputado(id_deputado)
+  resumo = gastos.get('resumo_despesas', {})
+
+  sorted_resumo = sorted(resumo.items(), key=lambda x: x[1], reverse=False)
+  # print(sorted_resumo)
+  categorias = [x[0] for x in sorted_resumo]
+  valores = [x[1] for x in sorted_resumo]
+
+  fig = go.Figure(data=[
+      go.Bar(
+          y=categorias,
+          x=valores,
+          orientation='h',
+          marker_color='#0047ab'
+      )
+  ])
+
+  fig.update_layout(
+      title=f"Resumo de Despesas - {gastos['nome']}",
+      xaxis=dict(
+          title="Valor (R$)",
+          dtick=100000,
+          tickformat=".2f"
+      ),
+      yaxis_title="Tipo de Despesa",
+      height=500,
+      width=900
+  )
+
+  return pio.to_json(fig)
+
+
+def get_deputado_presenca(id_deputado):
+    try:
+        with open(f'{DATA_PATH}/presenca_deputados/deputado_{id_deputado}.json', 'r', encoding='utf-8') as p:
+            presenca = json.load(p)
+
+        presencas, eventos =  presenca.get('total_presencas', 0), presenca.get('total_eventos', 0)
+        return presencas, eventos
+
+    except FileNotFoundError:
+        return 0, 0
